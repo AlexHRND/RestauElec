d8d2cc954184ec15b3b73ba13438b16e8033cffd
AlexHRND <44231932+AlexHRND@users.noreply.github.com>
2022-06-20 09:46:26 +0200

    progress

--- api_gateway/routes/index.js
+++ api_gateway/routes/index.js
@@ -4,25 +4,28 @@ const axios = require('axios')
 const registry = require('./registry.json')
 const fs = require('fs')
 const loadbalancer = require('../util/loadbalancer')
+const { TIMEOUT } = require('dns')
 
 
 router.all('/:apiName/:path', (req, res) => {
     const service = registry.services[req.params.apiName]
-        //if (service) {
-        //    if (!service.loadBalanceStrategy) {
-        //        service.loadBalanceStrategy = 'ROUND_ROBIN'
-        //        fs.writeFile('./routes/registry.json', JSON.stringify(registry), (error) => {
-        //             if (error) {
-        //                res.send("Couldn't write load balance strategy" + error)
-        //            }
-        //        })
-        //    }
-        //
-        //    const newIndex = loadbalancer[service.loadBalanceStrategy](service)
-        //const url = service.instances[newIndex].url
-    const url = "http://localhost:3001/"
-    console.log(url)
-    axios({
+    if (service) {
+        if (!service.loadBalanceStrategy) {
+            service.loadBalanceStrategy = 'ROUND_ROBIN'
+            fs.writeFile('./routes/registry.json', JSON.stringify(registry), (error) => {
+                if (error) {
+                    res.send("Couldn't write load balance strategy" + error)
+                }
+            })
+        }
+
+        const newIndex = loadbalancer[service.loadBalanceStrategy](service)
+        const url = service.instances[newIndex].url
+            //const url = "http://localhost:8000"
+        const date = new Date();
+        console.log(url + req.params.path + date.toISOString())
+
+        axios({
             method: req.method,
             url: url + req.params.path,
             headers: req.headers,
@@ -32,8 +35,9 @@ router.all('/:apiName/:path', (req, res) => {
         }).catch(error => {
             res.send("")
         })
-        //} else {
-        //    res.send("API Name doesn't exist")
-        //}
+    } else {
+        res.send("API Name doesn't exist")
+    }
 })
+
 module.exports = router
